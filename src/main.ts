2d3143303d7c04d8c096c83142b1fbea80ae437c
Alan <alanbianchi@id.uff.br>
2024-12-13 12:53:23 -0300

    inclusão do Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API de Estacionamento')
+    .setDescription('Documentação da API para gerenciamento de estacionamentos')
+    .setVersion('1.0')
+    .addBearerAuth() // Suporte a autenticação JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
